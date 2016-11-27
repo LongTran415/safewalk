2064acd0059d031dd8b5a265a08bde32c3711c2d
Long Tran <artsandcrass@gmail.com>
2016-11-27 14:27:01 -0800

    - Add computed property form email label, password label - Moved UIelements to be instance variables - Change background to darkgrey - Add background view for form - Add constraints to layout form - Add UI stackView to arrange view contents

--- SafeWalk/LoginViewController.swift
+++ SafeWalk/LoginViewController.swift
@@ -11,6 +11,20 @@ import UIKit
 class LoginViewController: UIViewController {
   
   //
+  // MARK: Instance variables
+  //
+  let formMinWidth: CGFloat = 200
+  let formMaxWidth: CGFloat = 300
+  let formHeight: CGFloat = 200
+  
+  var emailLabel: UILabel { return createFormLabel(text: "Email") }
+  let emailInput = UIInputView()
+  var passwordLabel: UILabel { return createFormLabel(text: "Password") }
+  let passwordInput = UIInputView()
+  let loginButton = UIButton(type: .infoDark)
+
+  
+  //
   // MARK: Default Overrides
   //
   
@@ -29,63 +43,89 @@ class LoginViewController: UIViewController {
   //
   
   private func setupView(){
-    self.view.backgroundColor = .lightGray
+    self.view.backgroundColor = .darkGray
     self.navigationItem.title = "Login"
   }
   
   private func setupForm(){
-    // Create a new view object, lay it out, and add to the page
-    //
-    // - Creates the view object
-    // - Set flag for layout using constraints
-    // - Create the constraints
-    // - Add to the subview
-    // - Add the constraints
+    // Create views
+    let bgView = createBackgroundView()
+    let formView = createFormView()
     
+    // Background View Constraints
+    let bgCenterX = NSLayoutConstraint(item: bgView, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 1)
+    let bgCenterY = NSLayoutConstraint(item: bgView, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 1)
     
-    // Start: Create view objects
-    let emailLabel = UILabel()
-    let emailInput = UIInputView()
-    let passwordLabel = UILabel()
-    let passwordInput = UIInputView()
-    let loginButton = UIButton(type: .infoDark)
-    // End: Create view objects
+    // Form View Constraints
+    let formWidth = NSLayoutConstraint(item: formView, attribute: .width, relatedBy: .equal, toItem: bgView, attribute: .width, multiplier: 1, constant: 1)
+    let formHeight = NSLayoutConstraint(item: formView, attribute: .height, relatedBy: .equal, toItem: bgView, attribute: .height, multiplier: 1, constant: 1)
+    let formTopPos = NSLayoutConstraint(item: formView, attribute: .top, relatedBy: .equal, toItem: bgView, attribute: .top, multiplier: 1, constant: 1)
+    let formLeftPos = NSLayoutConstraint(item: formView, attribute: .left, relatedBy: .equal, toItem: bgView, attribute: .left, multiplier: 1, constant: 1)
     
+    // Add subviews
+    bgView.addSubview(formView)
+    self.view.addSubview(bgView)
     
-    // Start: Configure view objects
-    emailLabel.text = "Email"
-    passwordLabel.text = "Password"
+    // Add constraints
+    bgView.addConstraints([formWidth, formHeight, formTopPos, formLeftPos])
+    self.view.addConstraints([bgCenterX, bgCenterY])
+  }
+  
+  
+  // 
+  // MARK: Helper functions
+  //
+  private func createFormLabel(text: String!) -> UILabel {
+    let view = UILabel()
+    view.text = text
+    view.sizeToFit()
     
-    emailLabel.sizeToFit()
-    passwordLabel.sizeToFit()
-    // End: Configure view objects
+    return view
+  }
+  
+  private func createFormRow(views: [UIView]) -> UIStackView {
+    let view = UIStackView(arrangedSubviews: views)
     
+    view.distribution = .equalSpacing
+    view.axis = .horizontal
+    view.spacing = 5.0
     
-    // Start: flag for layout constraints
-    emailLabel.translatesAutoresizingMaskIntoConstraints = false
-    emailInput.translatesAutoresizingMaskIntoConstraints = false
-    passwordLabel.translatesAutoresizingMaskIntoConstraints = false
-    passwordInput.translatesAutoresizingMaskIntoConstraints = false
-    loginButton.translatesAutoresizingMaskIntoConstraints = false
-    // End: flag for layout constraints
+    return view
+  }
+  
+  private func createFormView() -> UIView {
+    // Set up our view rows and subviews
+    let emailView = createFormRow(views: [emailLabel, emailInput]) as UIView
+    let passwordView = createFormRow(views: [passwordLabel, passwordInput]) as UIView
+    let subviews = [emailView, passwordView, loginButton]
+    
+    // Create the StackView
+    let formView = UIStackView(arrangedSubviews: subviews)
+    formView.translatesAutoresizingMaskIntoConstraints = false
+    formView.distribution = .equalSpacing
+    formView.axis = .vertical
+    formView.spacing = 5.0
 
-    
-    // Start: Create view constraints
-    
-    // End: Create view constraints
-    
-    
-    // Start: add to subview
-    self.view.addSubview(emailLabel)
-    self.view.addSubview(emailInput)
-    self.view.addSubview(passwordLabel)
-    self.view.addSubview(passwordInput)
-    self.view.addSubview(loginButton)
-    // End: add to subview
+    return formView
+  }
+  
+  private func createBackgroundView() -> UIView {
+    // Create a containing view to set a background color
+    let bgView = UIView()
+    bgView.backgroundColor = .lightGray
+    bgView.translatesAutoresizingMaskIntoConstraints = false
 
+    let bgMinWidth = NSLayoutConstraint(item: bgView, attribute: .width, relatedBy: .greaterThanOrEqual, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: formMinWidth)
+    let bgMaxWidth = NSLayoutConstraint(item: bgView, attribute: .width, relatedBy: .lessThanOrEqual, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: formMaxWidth)
+    let bgHeight = NSLayoutConstraint(item: bgView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: formHeight)
     
-    // Start: add constraints to view
-    // End: add constraints to view
-
+    bgView.addConstraints([bgMinWidth, bgMaxWidth, bgHeight])
+    
+    return bgView
   }
 }
+
+
+
+
+
